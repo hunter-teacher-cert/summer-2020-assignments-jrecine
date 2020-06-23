3d7120176cd990531437d4abd522a5b06cda62ed
jrecine <jrecine@schools.nyc.gov>
2020-06-23 16:38:57 -0400

    Ch06 Exercise 2

---
+++ pre05/Methods.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;	
+	
+	public class Methods {
+		
+	public static void main(String [] args) {
+
+	System.out.println(isDivisble(11,2));
+
+        }
+
+	public static boolean isDivisble(int n, int m) {
+
+	
+	if (n%m == 0) {
+
+			return true;
+
+		} else {
+
+		  return false;
+
+		}
+
+
+	}
+
+
+
+
+
+}
