961f8a4a95589fee377a688b00e7a79122fd55ef
jrecine <jrecine@schools.nyc.gov>
2020-06-23 22:38:03 -0400

    Ch07 7.9 Exercises - Exercise 3

---
+++ pre06/Loops.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Loops{
+	static int power(int x, int y) 
+    { 
+        if (y == 0) 
+            return 1; 
+        else if (y % 2 == 0) 
+            return power(x, y / 2) * power(x, y / 2); 
+        else
+            return x * power(x, y / 2) * power(x, y / 2); 
+    } 
+  
+    /* Program to test function power */
+    public static void main(String[] args) 
+    { 
+        int x = 5; 
+        int y = 3; 
+  
+        System.out.printf("%d", power(x, y)); 
+    } 
+} 
